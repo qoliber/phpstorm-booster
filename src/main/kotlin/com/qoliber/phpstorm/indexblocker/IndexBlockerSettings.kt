ad01a7f46511ac6f0898c3d28f8964cd249901b7
Jakub Winkler <jwinkler@qoliber.com>
2026-05-20 19:45:00 +0200

    feat: add IndexBlockerSettings persistent application service

---
+++ src/main/kotlin/com/qoliber/phpstorm/indexblocker/IndexBlockerSettings.kt
@@ -0,0 +1,47 @@
+package com.qoliber.phpstorm.indexblocker
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+@Service(Service.Level.APP)
+@State(name = "IndexBlocker", storages = [Storage("indexBlocker.xml")])
+class IndexBlockerSettings : PersistentStateComponent<IndexBlockerSettings.State> {
+
+    class State {
+        @JvmField var enabled: Boolean = true
+        @JvmField var blockedEntries: MutableList<String> = mutableListOf(
+            "node_modules",
+            "var",
+            "generated",
+            "dev",
+            "pub/static",
+            "Test*"
+        )
+    }
+
+    private var state = State()
+
+    override fun getState(): State = state
+
+    override fun loadState(loaded: State) {
+        XmlSerializerUtil.copyBean(loaded, state)
+    }
+
+    var enabled: Boolean
+        get() = state.enabled
+        set(value) { state.enabled = value }
+
+    var blockedEntries: List<String>
+        get() = state.blockedEntries.toList()
+        set(value) { state.blockedEntries = value.toMutableList() }
+
+    companion object {
+        @JvmStatic
+        fun getInstance(): IndexBlockerSettings =
+            ApplicationManager.getApplication().getService(IndexBlockerSettings::class.java)
+    }
+}
